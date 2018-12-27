47a0d5f27d31d5f1a725682a352d76457f96e0ee
dinghao188 <467682352@qq.com>
2018-12-27 17:22:28 +0800

    handing

--- Sort/include/insertionSort.h
+++ Sort/include/insertionSort.h
@@ -23,7 +23,7 @@ void InsertionSort<T>::shift(T a[],int start,int end,int n) {
 template <typename T>
 void InsertionSort<T>::sort(T a[],int start,int end) {
     for(int i = start;i < end;++i) {    //选择即将被插入的元素[start,end)
-        if(a[i] <= a[start]) {      //插到起始位置
+        if(a[i] < a[start]) {      //插到起始位置
             int tmp = a[i];
             shift(a,start,i,1);
             a[start] = tmp;
