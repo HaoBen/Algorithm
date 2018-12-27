70541381d71c2c362cec318c3367b7d63799ec8b
dinghao188 <467682352@qq.com>
2018-12-27 14:50:08 +0800

    das

---
+++ Sort/include/sort.h
@@ -0,0 +1,33 @@
+#ifndef SORT_H
+#define SORT_H
+
+/*
+    base class for sort
+*/
+#include <iostream>
+
+template <typename T>
+class Sort
+{
+protected:
+    void swap(T&a,T&b) {
+        T tmp = a;
+        a = b;
+        b = tmp;    
+    }
+    virtual ~Sort() {}
+public:
+    virtual void sort(T a[],int start,int end) = 0;
+    void show(T a[],int start,int end) {
+        while(start < end) std::cout<<a[start++]<<" ";
+        std::cout<<std::endl;
+    }
+    bool isSorted(T a[],int start,int end) {
+        while(start < end-1) {
+            if(a[start] > a[start+1]) return false;
+        }
+        return true;
+    }
+};
+
+#endif
