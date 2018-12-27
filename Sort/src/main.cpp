4b293f91ebe723b24ed487834a72e3541adc12ec
dinghao188 <467682352@qq.com>
2018-12-27 14:53:39 +0800

    modify

--- Sort/src/main.cpp
+++ Sort/src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "bubbleSort.h"
 
 using namespace std;
 
 int main() {
-    cout<<"Hello, World\n";
+    /*
+        produce a random array
+    */
+    int a[10];
+    srand(time(0));
+    for(size_t i = 0; i < 10; i++)
+    {
+        a[i] = rand() % 100;
+    }
+    
+    BubbleSort<int> bsort;
+    bsort.show(a,0,10);
+    bsort.sort(a,0,10);
+    bsort.show(a,0,10);
 }
