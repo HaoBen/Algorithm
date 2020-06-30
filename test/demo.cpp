64008e295e72f00f47942bc918c699a52f81b670
dinghao188 <467682352@qq.com>
2020-07-01 00:23:04 +0800

    呵呵

---
+++ test/demo.cpp
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+#include <iostream>
+
+TEST(Fuck, Fuck1)
+{
+    std::cout << "Fuck1..........." << std::endl;
+}
+
+TEST(Fuck, Fuck2)
+{
+    std::cout << "Fuck2..........." << std::endl;
+}
+
+TEST(Fuck, Fuck3)
+{
+    std::cout << "Fuck3..........." << std::endl;
+}
