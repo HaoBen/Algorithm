b5853b8105376758b4d97bbb2a05ae49faa31e6a
dinghao188 <467682352@qq.com>
2020-12-24 23:47:28 +0800

    add demo_test

--- tests/demo_test.cpp
+++ tests/demo_test.cpp
@@ -1,8 +1,28 @@
 #include "catch2/catch.hpp"
 
-TEST_CASE("this demonstrates how the tests work", "[demo_test]") {
-    CHECK(std::min(1,2) == 1);
-    CHECK(std::max(1,2) == 2);
+bool inc_order(const std::vector<int> &vec) {
+    for (int i = 0; i < vec.size()-1; ++i) {
+        if (vec[i] > vec[i+1]) return false;
+    }
+    return true;
+}
+bool dec_order(const std::vector<int> &vec) {
+    for (int i = 0; i < vec.size()-1; ++i) {
+        if (vec[i] < vec[i+1]) return false;
+    }
+    return true;
+}
 
-    REQUIRE(INT_MIN == (int)0x80000000);
+TEST_CASE("std sort", "[sort]") {
+    std::vector<int> vec = { 1, 3, 2, 4, 5, 6, 7, 8, 9, 0 };
+
+    SECTION("sort inc") {
+        std::sort(vec.begin(), vec.end());
+        REQUIRE(inc_order(vec));
+    }
+
+    SECTION("sort dec") {
+        std::sort(vec.begin(), vec.end(), [](int l, int r) { return !(l < r); });
+        REQUIRE(dec_order(vec));
+    }
 }
